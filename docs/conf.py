0ec7af5d8dc8231dd76eb961b51a86b323186cef
LAPTOP-SHURIK\Shurik <shurik179@gmail.com>
2020-03-29 17:16:51 -0400

    changing favicon

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ pygments_style = None
 html_theme = 'custom_sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
 html_logo = "images/roverwinglogo-gold.png"
-html_favicon = "images/roverwing.ico"
+html_favicon = "images/icon.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
